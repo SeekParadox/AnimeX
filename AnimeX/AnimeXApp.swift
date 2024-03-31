e65cd2c125f86f1fdf041bd79d7708fa7f84ef6f
Michael Waller <wallermichael14@gmail.com>
2024-03-31 13:55:16 -0400

    Initial Commit

---
+++ AnimeX/AnimeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeXApp.swift
+//  AnimeX
+//
+//  Created by Michael Waller on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
