f3125b2ecf9a8d5e1a7e43f233f1f2ce5e48f808
Michael Waller <wallermichael14@gmail.com>
2024-03-31 16:50:10 -0400

    Configured database connection

--- AnimeX/AnimeXApp.swift
+++ AnimeX/AnimeXApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import ParseSwift
 
 @main
 struct AnimeXApp: App {
+    
+    init() {
+        
+        let apiID = Bundle.main.infoDictionary?["APPLICATION_ID"] as? String ?? ""
+        let apiKey = Bundle.main.infoDictionary?["APPLICATION_KEY"] as? String ?? ""
+        
+        ParseSwift.initialize(
+            applicationId: apiID,
+            clientKey: apiKey,
+            serverURL: URL(string: "https://parseapi.back4app.com"
+                          )!)
+    }
+  
     var body: some Scene {
+    
         WindowGroup {
+    
             ContentView()
         }
     }
 }
+
